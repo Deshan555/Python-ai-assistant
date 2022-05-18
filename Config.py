ae5006ac56b9860b08fed76d2de9a72cd397e1d5
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-05-18 22:57:16 +0530

    add Config.py to project and add details

---
+++ Config.py
@@ -0,0 +1,21 @@
+# Open weather map details
+
+AUTH_KEY = 'ab46492c5e954d033a7e4a6afe58ccde'
+
+BASE_CITY = 'Rathnapura'
+
+# Database Details
+
+HOST_NAME = 'localhost'
+
+USER_NAME = 'root'
+
+PASSWORD = ''
+
+DATABASE_NAME = 'data_store'
+
+# Push Bullet API details
+
+API_KEY = 'o.SwB5RE7PBYNdAOSSjOBE60FRalj4MECx'
+
+
