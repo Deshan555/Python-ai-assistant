6e34ba7eae6010e284ecfa484b6210244974cef2
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-07-02 22:18:28 +0530

    improve screenshot taken accuracy

--- Screen_Shot.py
+++ Screen_Shot.py
@@ -2,10 +2,14 @@ import datetime
 
 import pywhatkit
 
+import Speak
+
 
 class screen_shot:
 
     def __init__(self):
+
+        Speak.Speak('Wait Five Seconds For ScreenShot capture')
 
         file_name = datetime.datetime.now().strftime('%H:%M %S')+'.JPG'
 
