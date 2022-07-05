be915368b98de75f419ceeacdc23d0c6e1d0a2e4
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-07-05 23:20:53 +0530

    add functionalities for IMDB.py script

---
+++ IMDB.py
@@ -0,0 +1,29 @@
+
+import pandas as pd
+
+import random
+
+import Speak
+
+
+def suggest_movies():
+
+    data = pd.read_csv("movies.csv")
+
+    choice: list = random.choice(data[['Title', 'Rating', 'Rank']].values)
+
+    return choice
+
+
+def talk():
+
+    data = suggest_movies()
+
+    Speak.Speak("I can suggest you "+data[0])
+
+    Speak.Speak("that movie hold "+str(data[2])+"th place on IMDB Top 250 Ranking List")
+
+    Speak.Speak(data[0]+"achieves"+str(data[1])+" ratings out of ten")
+
+
+talk()
