ad77b2277f4b76ee6cc43dc7267f8d6abcaf8736
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-07-06 10:22:52 +0530

    add Keyword_Extract.py :  that script help to extract keywords from specific sentences

---
+++ Keyword_Extract.py
@@ -0,0 +1,14 @@
+from rake_nltk import Rake
+
+
+def extract_keywords_from_text(txt: str):
+
+    rake_nltk_var = Rake()
+
+    text = txt
+
+    rake_nltk_var.extract_keywords_from_text(text)
+
+    keyword_extracted: list = rake_nltk_var.get_ranked_phrases()
+
+    print(keyword_extracted[-1])
