c62113bd4f9887db7087dfe79db76deae557445c
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-07-28 20:58:00 +0530

    add more flexible patterns for get answers

--- Google_Answers.py
+++ Google_Answers.py
@@ -6,52 +6,12 @@ import Speak
 
 import random
 
-from speech_recognition import Microphone, Recognizer, AudioFile, UnknownValueError, RequestError
-
 respond: list = ["According my knowledge", "Google said", "Best answer for your question is"]
 
 
-def voice_Text():
+def google_answers(question: str):
 
-    recognition = Recognizer()
-
-    mic = Microphone()
-
-    with mic:
-
-        recognition.adjust_for_ambient_noise(mic, duration=0.2)
-
-        print("Talk")
-
-        audio = recognition.record(mic, 5)
-
-    try:
-        recognized = recognition.recognize_google(audio)
-
-        print('You Said : ', recognized)
-
-        return recognized
-
-    except UnknownValueError:
-
-        print('Unable to Recognize audio')
-
-        return 'None'
-
-    except RequestError as error:
-
-        print('Error Detected : ', error)
-
-        return 'None'
-
-    return recognized
-
-
-while True:
-
-    Speak.Speak("What Is Your Question? I can help you find answers")
-
-    query = voice_Text().lower()
+    query = question.lower()
 
     url = "https://www.google.com/search?q=" + query.replace(" ", "+")
 
@@ -66,6 +26,7 @@ while True:
     Speak.Speak(random.choice(respond)+temp)
 
     print(temp)
+
 
 
 
